b24d1008d8bfd251a7f173a3a32be7ba1d08c190
diksonmg25 <diksonaraguari@gmail.com>
2025-03-07 19:09:32 -0300

    first commit

---
+++ src/test/java/TestUser.java
@@ -0,0 +1,41 @@
+// 0 - nome do pacote
+
+// 1 - bibliotecas
+
+// 2 - Classe
+
+import org.junit.jupiter.api.Test;
+
+public class TestUser {
+    // 2.1 atributos
+    static String ct = "application/json"; //content-type
+    static String uriUser= "https://petstore.swagger.io/v2/user"; 
+        // baseUrl + estrutura do endpoint da classe usuario - /user
+
+    // 2.2 funçoes e metodos
+    // 2.2.1 funçoes e metodos comuns/uteis
+    
+    // Funçao de leitura de json
+    public static String lerArquivoJson(String arquivoJson){
+        // TODO: Completar a leitura do arquivo
+        //return new String(Files)
+        return null;
+    }
+
+    // 2.2.2 metodos de teste
+
+    @Test
+    public void testPostPet(){
+        // carrregar os dados do arquivo json do pet
+        // TODO: Criar o metodo Post
+        // String jsonBody = 
+
+
+
+
+    }
+
+
+}
+
+
